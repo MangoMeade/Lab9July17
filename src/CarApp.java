bce5a5e0cca9213ebd78f9a887e0dcb9c508d8ac
Andres <aibarra@umich.edu>
2017-07-19 20:48:46 -0400

    added validators and comment

--- src/CarApp.java
+++ src/CarApp.java
@@ -1,32 +1,26 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 /**
  * Created by aibar on 7/18/2017.
  */
-public class CarApp {
+//Hold details about cars in an arraylist
+//Print list when user input ends.
+public class CarApp extends Validator{
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         Car car = new Car();
         String loop = "y";
 
-        LinkedList<Car> carList = new LinkedList<>();
+        ArrayList<Car> carList = new ArrayList<>();
         while (loop.equalsIgnoreCase("y")) {
-            System.out.println("Which company manufactures the car?");
-            car.setMake(scnr.nextLine());
-            System.out.println("What is the car model?");
-            car.setModel(scnr.nextLine());
-            System.out.println("When was the car manufactured?");
-            car.setYear(scnr.nextInt());
-            System.out.println("Price of the car?");
-            car.setPrice(scnr.nextDouble());
+            car.setMake(getString(scnr, "Which company manufactures the car?"));
+            car.setModel(getString(scnr, "What is the car model?"));
+            car.setYear(getInt(scnr, "When was the car manufactured?"));
+            car.setPrice(getDouble(scnr, "Price of the car?"));
             carList.add(car);
-            System.out.println("Another car?");
-            scnr.nextLine();
-            loop = scnr.nextLine();
+            loop = getString(scnr, "Another car?");
         }
-        System.out.println(carList);
 
         for (Car cars : carList) {
             System.out.println(cars.toString());
